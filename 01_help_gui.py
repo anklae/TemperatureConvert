8ab00ed80b69c2bd55a265430638bba4c08ee36d
anklae <20010@fraser.school.nz>
2024-04-05 09:13:37 +1300

    Finished most of the basic gui on 06_help_gui_01.py Should be finishing the rest of it within a few days Changed name  from 01_help_gui.py to 06_help_gui_01.py

--- 01_help_gui.py
+++ 01_help_gui.py
@@ -31,19 +31,19 @@ class DisplayHelp():
 
         self.help_frame.grid()
 
-        self.help_heading_label = Label(self.help_frame, bg =background, text ="Help / info",font=("Arial, 14, bold"))
+        self.help_heading_label = Label(self.help_frame, bg =background, text ="Help / info",font=("Arial", "14", "bold"))
         self.help_heading_label.grid(row=0)
 
-        help_text = "To use the program, simply enter the temperature" \
-                    "you wish to convert and then choose to convert 11"\
-                    "to either degrees Celsius (centigrade) or 11"\
-                    "Fahrenheit.. \n    \n "\
+        help_text = "To use the program, simply enter the temperature " \
+                    "you wish to convert and then choose to convert "\
+                    "to either degrees Celsius (centigrade) or "\
+                    "Fahrenheit..\n\n"\
                     "Note that -273 degrees C"\
                     "(-459 F) is absolute zero (the coldest possible "\
                     "temperature). If you try to convert a "\
-                    "temperature that is less than -273 degrees C, 11" \
-                    "you will get an error message. \n\n " \
-                    "To see your"\
+                    "temperature that is less than -273 degrees C, " \
+                    "you will get an error message. \n\n" \
+                    "To see your "\
                     "calculation history and export it to a text " \
                     "file, please click the 'History / Export' button."
         self.help_text_label = Label(self.help_frame, bg=background, text=help_text, wrap = 350, justify="left")
